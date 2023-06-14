1f94d8a4373a67f3784a7d88158fcb743cc67c12
Jude Alistair <jude55593@gmail.com>
2023-06-14 07:28:26 -0700

    simple square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,10 +6,9 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_row = []
         for num in row:
-            # Compute the square value of each integer and append it to the new row
+            # Compute the sq value of each integer, append it to the new row
             new_row.append(num ** 2)
         # Append the new row to the result matrix
         result.append(new_row)
 
     return (result)
-
